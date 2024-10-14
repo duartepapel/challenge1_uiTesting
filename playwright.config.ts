10e57463bb0ea4f4720737862b15dc4b63ab1521
duartepapel <papertech24@gmail.com>
2024-10-14 18:34:38 +0100

    Setup config and first steps

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  workers: 1,
+  timeout: 30 * 1000,
+  retries: 1,
+  reporter: 'html',
+  expect: {
+    timeout: 5000,
+  },
+  use: {
+    browserName: 'chromium',
+    headless: true,
+  },
+});
---
+++ tests/example.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('Should complete Challenge 1', async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/dist/')
+  const realUrl = page.url();
+  const expectedUrl = 'https://todomvc.com/examples/react/dist/'
+  expect(realUrl).toBe(expectedUrl);
+  const currentDate = new Date().toLocaleDateString;
+  await page.locator("#todo-input").fill(currentDate);
+  await page.pause();
+  console.log(currentDate);
+}
+)
