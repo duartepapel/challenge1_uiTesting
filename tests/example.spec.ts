ca26f157beb06711c9c506f1249ab48a57f25eb8
duartepapel <papertech24@gmail.com>
2024-10-14 19:49:47 +0100

    until step7

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,9 +5,24 @@ test('Should complete Challenge 1', async ({ page }) => {
   const realUrl = page.url();
   const expectedUrl = 'https://todomvc.com/examples/react/dist/'
   expect(realUrl).toBe(expectedUrl);
-  const currentDate = new Date().toLocaleDateString();
-  await page.locator("#todo-input").fill(currentDate);
-  await page.pause();
-  console.log(currentDate);
+  const today = new Date(); //variavel para o dia de hoje para reutilizar ao longo do teste
+  const currentDay = today.toLocaleDateString(); // criar objecto para o dia de hoje e converter data para uma string legivel
+  await page.locator("#todo-input").fill('TODO 1 - ' + currentDay); //concatenar strings e preencher input
+  await page.locator("#todo-input").press('Enter');
+  const toDoisListed = await page.locator("label[data-testid='todo-item-label']").isVisible(); //fazer store do selector do TODO
+  expect(toDoisListed).toBe(true); // validar que o selector existe na lista
+
+  const tommorow = new Date();
+  tommorow.setDate(today.getDate() + 1); //definir o dia de amanhã
+
+  const tommorowFormated = tommorow.toLocaleDateString();
+  await page.locator("#todo-input").fill('TODO 2 - ' + tommorowFormated);
+  await page.locator("#todo-input").press('Enter');
+
+  const currentDayToDo = await page.locator("[data-testid='todo-item-toggle']").nth(0);
+  await currentDayToDo.click();
+
+  const isStriked = await page.locator('[class="completed"]').isVisible();
+  expect(isStriked).toBe(true);
 }
 )
