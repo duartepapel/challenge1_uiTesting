3224874a4eb53404cc2ba903a30fbea4e1987100
duartepapel <papertech24@gmail.com>
2024-10-14 18:40:29 +0100

    config setup and first steps

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,7 +5,7 @@ test('Should complete Challenge 1', async ({ page }) => {
   const realUrl = page.url();
   const expectedUrl = 'https://todomvc.com/examples/react/dist/'
   expect(realUrl).toBe(expectedUrl);
-  const currentDate = new Date().toLocaleDateString;
+  const currentDate = new Date().toLocaleDateString();
   await page.locator("#todo-input").fill(currentDate);
   await page.pause();
   console.log(currentDate);
